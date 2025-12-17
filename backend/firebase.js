fc76fabfc6a67416ac574f7cf7fd9b89216e0212
Luis Carlo <115840553+LuisCarloMendoza@users.noreply.github.com>
2025-12-17 16:30:39 -0600

    Final Final Final Backend

--- backend/firebase.js
+++ backend/firebase.js
@@ -15,7 +15,7 @@ if (!serviceAccountEnv) {
     const __filename = fileURLToPath(import.meta.url);
     const _dirname = dirname(_filename);
 
-    const servicePath = join(__dirname, './rapicredit-f52a2-firebase-adminsdk-fbsvc-34bfa26aa4.json');
+    const servicePath = join(__dirname, 'rapicredit-f52a2-firebase-adminsdk-fbsvc-34bfa26aa4.json');
     const fileContents = readFileSync(servicePath, 'utf8');
     serviceAccountEnv = JSON.parse(fileContents);
 
